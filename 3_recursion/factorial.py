65be3fd9b79bc13c1e5c9ab5305ed116886c781b
arseniismpx <arseniit@gmail.com>
2023-11-23 00:19:45 +0100

    factorial realized with recurtion and test it

--- 3_recursion/factorial.py
+++ 3_recursion/factorial.py
@@ -1,4 +1,11 @@
 # using recursion to impllement factorial
 
 def factorial(x):
-    pass
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x-1)
+
+# test the factorial function
+print("{}! is {}".format(4, factorial(4)))
+print("{}! is {}".format(0, factorial(0)))
