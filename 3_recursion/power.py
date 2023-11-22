26a5cf21c6d94527611104d3f2c0294387e2dfe3
arseniismpx <arseniit@gmail.com>
2023-11-23 00:13:26 +0100

    add power function reqlized with recurtion, and test it

--- 3_recursion/power.py
+++ 3_recursion/power.py
@@ -1,4 +1,11 @@
 # using recursion to impllement power function
 
 def power(x, pwr):
-    pass
+    if pwr == 0:
+        return 1
+    else:
+        return x * power(x, pwr-1)
+
+# test the power function
+print("{} to the power of {} is {}".format(5,3,power(5,3)))
+print("{} to the power of {} is {}".format(1,5,power(1,5)))
